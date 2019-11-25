dc8c4b19368be79cc30f17c71ebf2aaf9dc5fd7a
Joakim Olsson <joakim.olsson@addskills.com>
2019-11-25 14:05:52 +0100

    Patch to old 'Get-VenuePlan'.

--- getVenueAttributeByVenueProperty.ps1
+++ getVenueAttributeByVenueProperty.ps1
@@ -2,7 +2,7 @@ function GetVenueAttributeByVenueProperty($venue_property) {
 	
 	$fetchHash = @{
 		Name="cint_venue_attribute"
-		Conditions=@{ a="cint_venue_attributeid"; o="eq"; value=$venue_property.cint_venue_attribute_id.id }
+		Conditions=@{ a="cint_venue_attributeid"; o="eq"; v=$venue_property.cint_venue_attribute_id.id }
 	}
 	
 	GetContent $fetchHash
