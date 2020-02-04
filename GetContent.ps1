8c30eacb47040de57b1292b3a979244434dc00ca
Joakim Olsson <joakim.olsson@addskills.com>
2020-02-04 14:30:20 +0100

    Removed the connection check from GetContent

--- GetContent.ps1
+++ GetContent.ps1
@@ -2,31 +2,6 @@
 #requires -Modules AMSoftware.CRM, ACGCore
 . "$PSScriptRoot\FetchHashToFetchXML.ps1"
 
-try {
-	$null = Get-CrmOrganization
-	Write-Debug "Already connected to CRM."
-} catch {
-	$ex = $_
-	if ($_.Exception.message -eq "Not connected with a CRM Deployment. Run Connect-CrmDeployment.") {
-		$runasFile = "$PSScriptRoot\runas"
-		if (Test-Path $runasFile) {
-			try {
-				$cred = Load-Credential $runasFile
-				$null = Connect-CrmDeployment -DiscoveryUrl "http://crm.addskills.se" -Credential $cred
-				$conn = Connect-CrmOrganization cornerstone
-				"Connected to CRM using runas-credentials (as '{0}')." -f $cred.Username | Write-Debug
-				$conn | Out-string | Write-Debug
-			} catch {
-				throw $_
-			}
-		} else {
-			throw $ex
-		}
-	} else {
-		throw $ex
-	}
-}
-
 function GetContent {
 	[CmdletBinding()]
 	param(
